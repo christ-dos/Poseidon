88c4df63f56979024ec3a7b5a4eae436768a0d73
Christine Duarte <christine_dos_santos@hotmail.fr>
2021-09-09 10:30:52 +0200

    implementation entity BidList

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/BidList.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/BidList.java
@@ -1,5 +1,9 @@
 package com.nnk.springboot.domain;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.beans.factory.annotation.Required;
 
 import javax.persistence.*;
@@ -9,7 +13,42 @@ import java.sql.Date;
 import java.sql.Timestamp;
 
 @Entity
-@Table(name = "bidlist")
+@Setter
+@Getter
+@Builder
+@NoArgsConstructor
+@Table(name = "BidList")
 public class BidList {
-    // TODO: Map columns in data table BIDLIST with corresponding java fields
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "BidListId", nullable = false)
+    private Integer bidListId;
+
+    private  String account;
+    private  String type;
+    private  Double bidQuantity;
+    private  Double askQuantity;
+    private  Double bid;
+    private  Double ask;
+    private String benchmark;
+    private Timestamp bidListDate;
+    private String commentary;
+    private String security;
+    private String status;
+    private String trader;
+    private String book;
+    private String creationName;
+    private Timestamp creationDate;
+    private String revisionName;
+    private Timestamp revisionDate;
+    private String dealName;
+    private String dealType;
+    private String sourceListId;
+    private String side;
+
+    public BidList(String account_test, String type_test, double v) {
+    }
+
+// TODO: Map columns in data table BIDLIST with corresponding java fields
 }
