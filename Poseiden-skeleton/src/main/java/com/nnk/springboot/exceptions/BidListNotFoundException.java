41d4250b5e0648b0a0b355a2336131300a428016
Christine Duarte <christine_dos_santos@hotmail.fr>
2021-09-20 14:13:52 +0200

    writing javadoc for package exceptions

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/exceptions/BidListNotFoundException.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/exceptions/BidListNotFoundException.java
@@ -1,6 +1,16 @@
 package com.nnk.springboot.exceptions;
 
+/**
+ * Class that handles exception when the bidList is not found
+ *
+ * @author Christine Duarte
+ */
 public class BidListNotFoundException extends RuntimeException {
+    /**
+     * Constructor
+     *
+     * @param message A string containing the message that is sent if the exception in handling
+     */
     public BidListNotFoundException(String message) {
         super(message);
     }
--- Poseiden-skeleton/src/main/java/com/nnk/springboot/exceptions/RatingNotFoundException.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/exceptions/RatingNotFoundException.java
@@ -1,6 +1,16 @@
 package com.nnk.springboot.exceptions;
 
+/**
+ * Class that handles exception when the rating is not found
+ *
+ * @author Christine Duarte
+ */
 public class RatingNotFoundException extends RuntimeException{
+    /**
+     * Constructor
+     *
+     * @param message A string containing the message that is sent if the exception in handling
+     */
     public RatingNotFoundException(String message) {
         super(message);
     }
